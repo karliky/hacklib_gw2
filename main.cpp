f4eab6d7b1e3d9ed84419f389b4353376d88a09d
hairys <githairys@zcnoc.net>
2016-03-09 00:54:32 -0500

    Fix nesting from previous commit.

--- main.cpp
+++ main.cpp
@@ -650,13 +650,11 @@ void Gw2HackMain::GameHook()
                                 if (pCharData->pAgentData->pAgent) {
                                     hl::ForeignClass transform = pCharData->pAgentData->pAgent.get<void*>(m_pubmems.agentTransform);
                                     if (transform) {
-                                        if (transform) {
-                                            if (pCharData->isPlayer) {
-                                                pCharData->pAgentData->speed = transform.get<float>(m_pubmems.agtransSpeed);
-                                            } else {
-                                                pCharData->pAgentData->rot = atan2(transform.get<float>(m_pubmems.npc_agtransRY), transform.get<float>(m_pubmems.npc_agtransRX));
-                                                pCharData->pAgentData->speed = transform.get<float>(m_pubmems.npc_agtransSpeed);
-                                            }
+                                        if (pCharData->isPlayer) {
+                                            pCharData->pAgentData->speed = transform.get<float>(m_pubmems.agtransSpeed);
+                                        } else {
+                                            pCharData->pAgentData->rot = atan2(transform.get<float>(m_pubmems.npc_agtransRY), transform.get<float>(m_pubmems.npc_agtransRX));
+                                            pCharData->pAgentData->speed = transform.get<float>(m_pubmems.npc_agtransSpeed);
                                         }
                                     }
                                 }
